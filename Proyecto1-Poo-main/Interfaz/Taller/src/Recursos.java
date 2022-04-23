dd1d404dad4b21ed6a5727248a1cf01038f2a682
Yess Solano <90736001+yessmsr@users.noreply.github.com>
2022-04-22 19:40:26 -0600

    Update Recursos.java

--- Proyecto1-Poo-main/Interfaz/Taller/src/Recursos.java
+++ Proyecto1-Poo-main/Interfaz/Taller/src/Recursos.java
@@ -15,5 +15,35 @@ public class Recursos extends Objetos{
         lista[6]=recurso7;
     }
     public void acciones(){
+        while(lista.length!=0){
+            if(abejaDef1==recurso1||abejaDef2==recurso1||abeja4==recurso1){
+                casillas[11][1].setText(" ");
+                //De está forma se refleja que el recurso se recolecto
+            }
+            else if(abeja5==recurso2||abejaDef2==recurso2||abeja4==recurso2){
+                casillas[23][3].setText(" ");
+            }
+            else if(abejaDef1==recurso3||abeja6==recurso3||abejaDef3==recurso3){
+                casillas[9][5].setText(" ");
+            }
+            else if(abejaDef1==recurso4||abejaDef2==recurso4||abeja4==recurso4){
+                casillas[23][10].setText(" ");
+            }
+            else if(abeja4==recurso5||abeja5==recurso5||abeja6==recurso5){
+                casillas[23][3].setText(" ");
+            }
+            else if(abejaDef1==recurso6||abejaDef2==recurso6||abejaDef3==recurso6){
+                casillas[17][15].setText(" ");
+            }
+            else if(abeja4==recurso7||abeja5==recurso7||abeja6==recurso7){
+                casillas[9][27].setText(" ");
+            }
+            else if(abejaDef1==recurso8||abejaDef2==recurso8||abeja4==recurso8){
+                casillas[1][9].setText(" ");
+            }
+            else if(abeja4==recurso7||abeja5==recurso7||abeja6==recurso7){
+                casillas[19][23].setText(" ");
+            }
+        }
     }
 }
