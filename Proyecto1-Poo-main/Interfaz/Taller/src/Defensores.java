1b80610d5f9df74e379ebf98b81fa87d6736526e
Yess Solano <90736001+yessmsr@users.noreply.github.com>
2022-04-26 12:58:47 -0600

    Update Defensores.java

--- Proyecto1-Poo-main/Interfaz/Taller/src/Defensores.java
+++ Proyecto1-Poo-main/Interfaz/Taller/src/Defensores.java
@@ -1,10 +1,18 @@
+import javax.swing.JLabel;
+import java.awt.*;
+
 public class Defensores extends AgenteBase {
-    public int cantidad;
-    public int pos;
     
-    public Defensores(){}
-    public Defensores(int cantidad,int pos){
-        this.cantidad=cantidad;
+    public Defensores(int pos, JLabel[] lista){
+        super( pos, lista);
         this.pos=pos;
+    }
+    public static void acciones1(){
+        casillas[0][0].setText("  🐝");casillas[0][0].setForeground(Color.orange); abejaDef1=casillas[0][0];
+        casillas[10][3].setText("  🐝"); casillas[10][3].setForeground(Color.orange); abejaDef2=casillas[10][3];
+        casillas[9][0].setText("  🐝");casillas[9][0].setForeground(Color.orange); abejaDef3=casillas[9][0];
+        lista[0]=abejaDef1;
+        lista[1]=abejaDef2;
+        lista[2]=abejaDef3;
     }
 }
