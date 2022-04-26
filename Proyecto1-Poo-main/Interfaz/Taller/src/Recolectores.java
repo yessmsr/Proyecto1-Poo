c69cd22d3cdcbff5b326bc57e8b2ccd87354f9d3
Yess Solano <90736001+yessmsr@users.noreply.github.com>
2022-04-26 12:59:22 -0600

    Update Recolectores.java

--- Proyecto1-Poo-main/Interfaz/Taller/src/Recolectores.java
+++ Proyecto1-Poo-main/Interfaz/Taller/src/Recolectores.java
@@ -1,8 +1,15 @@
 public class Recolectores extends AgenteBase {
-    public int cantidad;
-    public int pos;
 
-    
-
-    
+    public Recolectores(int cantidad, int pos){
+        super(pos, lista);
+        this.pos=pos;
+    }
+    public static void acciones1(){
+        casillas[9][3].setText("  🐝");abeja4=casillas[9][3];
+        casillas[19][9].setText("  🐝");;abeja5=casillas[19][9];
+        casillas[5][2].setText("  🐝"); abeja6=casillas[5][2];
+        lista[0]=abeja4;
+        lista[1]=abeja5;
+        lista[2]=abeja6;
+    }
 }
