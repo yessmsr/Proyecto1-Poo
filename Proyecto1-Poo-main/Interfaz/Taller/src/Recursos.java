577fe263884459cf69b927ebdc77783ae1c73f49
Yess Solano <90736001+yessmsr@users.noreply.github.com>
2022-04-26 13:01:31 -0600

    Update Recursos.java

--- Proyecto1-Poo-main/Interfaz/Taller/src/Recursos.java
+++ Proyecto1-Poo-main/Interfaz/Taller/src/Recursos.java
@@ -1,11 +1,48 @@
 import javax.swing.JLabel;
+import java.awt.*;
 
 public class Recursos extends Objetos{
-
+    
     public Recursos(JLabel[] lista,JLabel recurso1){
         super(lista, recurso1);
     }
-    public void agregarRecursos(){
+    public static void agregarRecursos(){
+        casillas[10][0].setText("   🌸"); casillas[10][0].setForeground(Color.red);casillas[11][0].setText("   🌸");
+        casillas[11][0].setForeground(Color.red);
+        casillas[12][1].setText("   🌸"); casillas[12][1].setForeground(Color.red);
+        casillas[11][1].setText("   🌸"); casillas[11][1].setForeground(Color.red);recurso1=casillas[11][1];
+        casillas[0][5].setText("   🌸"); casillas[0][5].setForeground(Color.red);
+        casillas[24][3].setText("   🌸"); casillas[24][3].setForeground(Color.red);
+        casillas[24][4].setText("   🌸"); casillas[24][4].setForeground(Color.red);
+        casillas[23][3].setText("   🌸"); casillas[23][3].setForeground(Color.red);recurso2=casillas[23][3];
+        casillas[10][5].setText("   🌸"); casillas[10][5].setForeground(Color.red);
+        casillas[10][6].setText("   🌸"); casillas[10][6].setForeground(Color.red);
+        casillas[11][6].setText("   🌸"); casillas[11][6].setForeground(Color.red);
+        casillas[9][5].setText("   🌸"); casillas[9][5].setForeground(Color.red);recurso3=casillas[9][5];
+        casillas[0][12].setText("   🌸"); casillas[0][12].setForeground(Color.red);
+        casillas[24][11].setText("   🌸"); casillas[24][11].setForeground(Color.red);
+        casillas[24][10].setText("   🌸"); casillas[24][10].setForeground(Color.red);
+        casillas[23][10].setText("   🌸"); casillas[23][10].setForeground(Color.red);recurso4=casillas[23][10];
+        casillas[18][15].setText("   🌸"); casillas[18][15].setForeground(Color.red);
+        casillas[18][16].setText("   🌸"); casillas[18][16].setForeground(Color.red);
+        casillas[19][16].setText("   🌸"); casillas[19][16].setForeground(Color.red);
+        casillas[17][15].setText("   🌸"); casillas[17][15].setForeground(Color.red);recurso5=casillas[17][15];
+        casillas[12][20].setText("   🌸"); casillas[12][20].setForeground(Color.red);
+        casillas[12][21].setText("   🌸"); casillas[12][21].setForeground(Color.red);
+        casillas[13][21].setText("   🌸"); casillas[13][21].setForeground(Color.red);
+        casillas[11][20].setText("   🌸"); casillas[11][20].setForeground(Color.red);recurso6=casillas[11][20];
+        casillas[10][27].setText("   🌸"); casillas[10][27].setForeground(Color.red);
+        casillas[10][28].setText("   🌸"); casillas[10][28].setForeground(Color.red);
+        casillas[11][28].setText("   🌸"); casillas[11][28].setForeground(Color.red);
+        casillas[9][27].setText("   🌸"); casillas[9][27].setForeground(Color.red);recurso7=casillas[9][27];
+        casillas[3][10].setText("   🌸"); casillas[3][10].setForeground(Color.red);
+        casillas[2][10].setText("   🌸"); casillas[2][10].setForeground(Color.red);
+        casillas[2][9].setText("   🌸"); casillas[2][9].setForeground(Color.red);
+        casillas[1][9].setText("   🌸"); casillas[1][9].setForeground(Color.red);recurso8=casillas[1][9];
+        casillas[21][24].setText("   🌸"); casillas[21][24].setForeground(Color.red);
+        casillas[20][24].setText("   🌸"); casillas[20][24].setForeground(Color.red);
+        casillas[20][23].setText("   🌸"); casillas[20][23].setForeground(Color.red);
+        casillas[19][23].setText("   🌸"); casillas[19][23].setForeground(Color.red);recurso9=casillas[19][23];
         lista[0]=recurso1;
         lista[1]=recurso2;
         lista[2]=recurso3;
@@ -16,36 +53,5 @@ public class Recursos extends Objetos{
         lista[7]=recurso8;
         lista[8]=recurso9;
     }
-    public void acciones(){
-        while(lista.length!=0){
-            if(abejaDef1==recurso1||abejaDef2==recurso1||abeja4==recurso1){
-                casillas[11][1].setText(" ");
-                //De está forma se refleja que el recurso se recolecto
-            }
-            else if(abeja5==recurso2||abejaDef2==recurso2||abeja4==recurso2){
-                casillas[23][3].setText(" ");
-            }
-            else if(abejaDef1==recurso3||abeja6==recurso3||abejaDef3==recurso3){
-                casillas[9][5].setText(" ");
-            }
-            else if(abejaDef1==recurso4||abejaDef2==recurso4||abeja4==recurso4){
-                casillas[23][10].setText(" ");
-            }
-            else if(abeja4==recurso5||abeja5==recurso5||abeja6==recurso5){
-                casillas[23][3].setText(" ");
-            }
-            else if(abejaDef1==recurso6||abejaDef2==recurso6||abejaDef3==recurso6){
-                casillas[17][15].setText(" ");
-            }
-            else if(abeja4==recurso7||abeja5==recurso7||abeja6==recurso7){
-                casillas[9][27].setText(" ");
-            }
-            else if(abejaDef1==recurso8||abejaDef2==recurso8||abeja4==recurso8){
-                casillas[1][9].setText(" ");
-            }
-            else if(abeja4==recurso9||abeja5==recurso9||abeja6==recurso9){
-                casillas[19][23].setText(" ");
-            }
-        }
-    }
+    
 }
